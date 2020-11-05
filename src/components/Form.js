bff0966db0cddb38e7f6f4169e8d3ebdab238bff
David Portilla <david_portilla@hotmail.com>
2020-11-05 10:16:47 -0500

    setting and updating the state form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Form = () => {
+
+  const [ appointment, updateAppointment ] = useState({
+    petName: '',
+    owner: '',
+    date: '',
+    hour: '',
+    symptoms: ''
+  })
+
+  const updateState = (e) => {
+    updateAppointment({
+      ...appointment,
+      [ e.target.name ]: e.target.value
+    })
+  }
+
+  const { petName, owner, date, hour, symptoms } = appointment
+
+  const submitAppointment = (e) => {
+    e.preventDefault()
+    console.log('sending form')
+  }
+
   return (
     <>
       <h2>add appointment</h2>
-      <form>
+      <form
+        onSubmit={ submitAppointment }
+      >
 
         <label>Name</label>
         <input
           type="text"
-          name="name"
+          name="petName"
           className="u-full-width"
           placeholder="Pet name"
+          onChange={ updateState }
+          value={ petName }
         />
 
         <label>Owner</label>
@@ -20,6 +47,8 @@ export const Form = () => {
           name="owner"
           className="u-full-width"
           placeholder="Owner name"
+          onChange={ updateState }
+          value={ owner }
         />
 
         <label>Date</label>
@@ -27,6 +56,8 @@ export const Form = () => {
           type="date"
           name="date"
           className="u-full-width"
+          onChange={ updateState }
+          value={ date }
         />
 
         <label>Hour</label>
@@ -34,12 +65,16 @@ export const Form = () => {
           type="time"
           name="hour"
           className="u-full-width"
+          onChange={ updateState }
+          value={ hour }
         />
 
         <label>Symptoms</label>
         <textarea
           name="symptoms"
           className="u-full-width"
+          onChange={ updateState }
+          value={ symptoms }
         ></textarea>
 
         <button
